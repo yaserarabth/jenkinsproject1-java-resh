3464ec49886ec268c235165dfb904903311a8435
Yaser arabth <101051123+yaserarabth@users.noreply.github.com>
2026-04-08 15:43:50 +0530

    Update print statement to include '-webhook'

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!new changetttt ");
+        System.out.println("Hello, Jenkins!new changetttt-webhook ");
     }
 
     public int add(int a, int b) {
